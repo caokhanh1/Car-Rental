4fbcd769898eeb7743223ede7876fa1761d7d9d1
caokhanh1 <daxxua911@gmail.com>
2024-08-02 15:57:07 +0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import Signup from './pages/Signup'
+import About from './pages/About'
+import Profile from './pages/Profile'
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/> } />
+        <Route path="/sign-in" element={<SignIn/> } />
+        <Route path="/sign-up" element={<Signup/> } />
+        <Route path="/about" element={<About/> } />
+        <Route path="/profile" element={<Profile/> } />
+      </Routes>
+  </BrowserRouter>
+  )
+}
