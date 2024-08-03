df7057c3680f06577c5f05dcb5d93e3ba185c7a7
caokhanh1 <daxxua911@gmail.com>
2024-08-03 21:57:25 +0700

    khanh/profile

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -24,6 +24,19 @@ export default function Signup() {
           className="border p-3 rounded-lg"
           id="password"
         />
+         <input
+          type="address"
+          placeholder="address"
+          className="border p-3 rounded-lg"
+          id="address"
+        />
+        <input
+          type="phone"
+          placeholder="phone"
+          className="border p-3 rounded-lg"
+          id="phone"
+        />
+
 
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
          Sign Up
